6577c70014e00cb3f6f7503f570e47264765154b
DevDave <info@devdave.com>
2014-03-29 17:12:20 -0700

    the files that where supposed to be in the last commit

---
+++ Web/Scripts/HomeModule.js
@@ -0,0 +1,27 @@
+﻿var HomeModule = function () {
+    var selectors = {
+        contactButton: '',
+        aboutButton: ''
+    }
+
+    var urls = {
+        contactUrl: '',
+        aboutUrl: ''
+    }
+
+    var init = function () {
+        $(selectors.contactButton).click(function () {
+            window.location = urls.contactUrl;
+        });
+
+        $(selectors.aboutButton).click(function () {
+            window.location = urls.aboutUrl;
+        });
+    }
+
+    return {
+        selectors: selectors,
+        urls: urls,
+        init: init,
+    }
+}();
